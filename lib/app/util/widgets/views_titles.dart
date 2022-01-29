47ac01b4dc291ea3f56689537315aee58277d1a4
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-01-28 21:39:49 -0300

    feat: :lipstick: botão de adicionar opcional

--- lib/app/util/widgets/views_titles.dart
+++ lib/app/util/widgets/views_titles.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-Widget viewsTitles({required String title}) {
+Widget viewsTitles({required String title,bool? isButtonAdd}) {
   return Padding(
     padding: const EdgeInsets.only(bottom: 20.0),
-    child:  Text(
-      title,
-      style: const TextStyle(
-        fontSize: 26,
-        fontWeight: FontWeight.w300,
-      ),
+    child:  Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        (isButtonAdd == true) ? GestureDetector(
+          child:  Padding(
+            padding:  const EdgeInsets.only(left: 3.0),
+            child: Icon(Icons.add_circle,color: Colors.green[400], size: 24,), 
+          ),
+        ) : Container()
+      ],
     ),
   );
 }
