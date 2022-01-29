4faf6d6f029940f5e6f20e5de0cac692b94f69e3
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-01-28 22:21:56 -0300

    feat: :lipstick: Adicionando rota ao clicar no card da lista.

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:rv_crud_challenger/app/modules/home/views/product_details_view.dart';
+import 'package:rv_crud_challenger/app/modules/home/widgets/dialog.dart';
+import 'package:rv_crud_challenger/app/util/widgets/card_for_product_list.dart';
 import 'package:rv_crud_challenger/app/util/widgets/views_titles.dart';
 
 import '../controllers/home_controller.dart';
@@ -11,46 +14,52 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.grey[200],
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 50, 8, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            viewsTitles(title: 'Lista de Produtos',),
-          ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey[200],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 50, 8, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              viewsTitles(
+                title: 'Lista de Produtos',
+                isButtonAdd: true,
+              ),
+              Expanded(
+                child: SizedBox(
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) => GestureDetector(
+                      onTap: () => Get.to(const ProductDetailsView(),),
+                      child: CardForProductList(
+                        title: 'teste',
+                        subTitle: 'asdasdsadasdasdasdasdasdasdasdasdasdasd',
+                        price: '50,00',
+                        onTapIcon: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return alertDialogProduct(
+                                context,
+                                controller,
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ),);
+    );
   }
 }
 
 
-// Container(
-//       width: double.infinity,
-//       height: double.infinity,
-//       color: Colors.grey[200],
-//       child: Padding(
-//         padding: const EdgeInsets.fromLTRB(8, 50, 8, 0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.only(bottom: 20.0),
-//               child: const Text(
-//                 'Lista produto',
-//                 style: TextStyle(
-//                   fontSize: 26,
-//                   fontWeight: FontWeight.w300,
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     ),);
