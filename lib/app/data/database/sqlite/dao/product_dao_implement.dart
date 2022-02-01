f461e1c2fe86b1d794ff895105761b448978f970
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-02-01 10:30:16 -0300

    feat: :sparkles: implementação do DAO
    contém os métodos do CRUD implementados

---
+++ lib/app/data/database/sqlite/dao/product_dao_implement.dart
@@ -0,0 +1,40 @@
+import 'package:rv_crud_challenger/app/data/database/sqlite/connection.dart';
+import 'package:rv_crud_challenger/app/data/domain/entities/product.dart';
+import 'package:rv_crud_challenger/app/data/domain/interfaces/product_dao.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class ProductDAOImplement implements ProductDAO {
+  Database? _db;
+
+  @override
+  Future<List<Product>> find() async {
+    _db = await Connection.get();
+    List<Map<String, dynamic>> result = await _db!.query('product');
+    List<Product> list = List.generate(result.length, (index) {
+      var linha = result[index];
+      return Product.fromMap(linha);
+    });
+    return list;
+  }
+
+  @override
+  remove(int id) async {
+    _db = await Connection.get();
+    String sql = 'DELETE FROM product WHERE id = ?';
+    _db!.rawDelete(sql, [id]);
+  }
+
+  @override
+  save(Product product) async {
+    _db = await Connection.get();
+    String sql;
+    if (product.id == null) {
+      sql = 'INSERT INTO product (name,details,photo) VALUE (?,?,?)';
+      _db!.rawInsert(sql,[product.name,product.details,product.photo]);
+
+    }else {
+      sql = 'UPDATE product SET name = ?, details = ?, photo = ? WHERE id = ?';
+      _db!.rawInsert(sql,[product.name,product.details,product.photo,product.id]);
+    }
+  }
+}
