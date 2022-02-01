ecb957d693e7922bfd4b93b231d578038602efb4
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-02-01 12:47:07 -0300

    feat: :sparkles: adicionando método para navegar entre editar e criar um novo produto

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rv_crud_challenger/app/data/domain/entities/product.dart';
 import 'package:rv_crud_challenger/app/data/domain/services/product_service.dart';
@@ -6,25 +5,56 @@ import 'package:rv_crud_challenger/app/modules/home/views/update_view.dart';
 
 class HomeController extends GetxController {
   final _service = ProductService();
+   bool nameIsValid = false;
+   bool detailsIsValid = false;
+  // bool? _emailIsValid;
+
+  //bool get isValid =>  _nameIsValid! && _detailsIsValid!;
 
   var list = Future.value(<Product>[]).obs;
-  HomeController(){
+  HomeController() {
+    
     refreshList();
   }
 
-  refreshList(){
+  refreshList() {
     list.value = _service.find();
   }
 
-  save(Product product){
+  save(Product product) {
     _service.save(product);
+    refreshList();
+
   }
 
-  remove(int id){
+  remove(int id) {
     _service.remove(id);
     refreshList();
   }
-  goToForm(BuildContext context,[Product? product]){
-    // Get.to(UpdateView(),arguments: product).then(refreshList);
+
+  goToForm([Product? product]) {
+    Get.to(const UpdateView(), arguments: product);
   }
+
+   String validateName(String? name) {
+    try {
+      _service.validateName(name);
+      nameIsValid = true;
+      return '';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  String validateDetails(String? details) {
+    try {
+      _service.validateDetails(details);
+      detailsIsValid = true;
+      return '';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  validatePhoto() {}
 }
