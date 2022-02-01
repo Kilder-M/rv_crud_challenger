db9dbb4d9b2bb3560e9b3bf743036c979f61886a
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-02-01 12:45:27 -0300

    feat: :sparkles: Colocando atributo id na conversao de json para dart

--- lib/app/data/domain/entities/product.dart
+++ lib/app/data/domain/entities/product.dart
@@ -8,6 +8,7 @@ class Product{
   Product({this.details,this.id,this.name,this.photo});
 
    factory Product.fromMap(Map<String, dynamic> json) => Product(
+        id: json['id'],
         name: json["name"],
         details: json["details"],
         photo: json["photo"],
