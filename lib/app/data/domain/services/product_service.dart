0b5211eb1a944616e3e4f5921aea0c813a95bbee
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-02-01 10:32:25 -0300

    feat: :sparkles: serviço de produto

---
+++ lib/app/data/domain/services/product_service.dart
@@ -0,0 +1,46 @@
+import 'package:rv_crud_challenger/app/data/database/sqlite/dao/product_dao_implement.dart';
+import 'package:rv_crud_challenger/app/data/domain/entities/product.dart';
+import 'package:rv_crud_challenger/app/data/domain/exceptions/domain_layer_exception.dart';
+
+class ProductService{
+  final _dao = ProductDAOImplement();
+    save(Product product){
+    validateNome(product.name);
+    validateDescricao(product.details);
+    _dao.save(product);
+
+  }
+
+  remove(int id){
+    _dao.remove(id);
+  }
+
+  Future<List<Product>>find()async{
+    return _dao.find();
+  }
+  validateNome(String? name){
+   var max = 50;
+   var min =3;
+
+   if(name == null){
+     throw  DomainLayerException("O nome é obrigatório.");
+
+   }else if(name.length > max){
+     throw  DomainLayerException("O nome deve possuir no máximo $max caracters.");
+
+   }else if(name.length < min){
+     throw  DomainLayerException("O nome deve possuir no mínimo $min caracters.");
+   }
+    
+ }
+
+ validateDescricao(String? email){
+   if(email == null){
+     throw  DomainLayerException("O email é obrigatório.");
+   }else if(!email.contains('@')){
+     throw  DomainLayerException('O email deve possuir @');
+   }
+ }
+
+
+}
