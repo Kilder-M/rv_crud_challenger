1512ee4029b997d8dc5171edf5682aff376504fe
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-02-02 11:25:49 -0300

    refactor: :fire: Alterando nome das variaveis e removendo codigos

--- lib/app/data/database/sqlite/connection.dart
+++ lib/app/data/database/sqlite/connection.dart
@@ -1,12 +1,12 @@
 import 'package:path/path.dart';
-import 'package:rv_crud_challenger/app/data/database/sqlite/script.dart';
+import 'package:rv_crud_challenger/app/data/database/sqlite/script_product.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
 class Connection {
   static Database? _db;
 
-  static Future<Database> get() async {
+  static Future<Database> getProduct() async {
     if (_db == null) {
       String path = join(await getDatabasesPath(), 'banco_product');
       deleteDatabase(path);
@@ -15,7 +15,6 @@ class Connection {
         version: 1,
         onCreate: (db,v){
           db.execute(createTable);
-          db.execute(insert);
         } 
       );
     }
